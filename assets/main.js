74980a118c5056bd50307f4a232bc74243907784
Adex-TM <ad3x96tm@gmail.com>
2025-04-21 09:29:03 +0300

    Add files via upload

---
+++ assets/main.js
@@ -0,0 +1,71 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const dropdowns = document.querySelectorAll(".dropdown");
+  
+    dropdowns.forEach(dropdown => {
+      const button = dropdown.querySelector(".dropbtn");
+      const content = dropdown.querySelector(".dropdown-content");
+  
+      button.addEventListener("click", function (e) {
+        e.preventDefault();
+  
+        dropdowns.forEach(d => {
+          if (d !== dropdown) {
+            d.querySelector(".dropdown-content").classList.remove("show");
+            d.querySelector(".dropbtn").classList.remove("active");
+          }
+        });
+  
+        content.classList.toggle("show");
+        button.classList.toggle("active");
+      });
+    });
+  
+    window.addEventListener("click", function (e) {
+      if (!e.target.closest(".dropdown")) {
+        dropdowns.forEach(dropdown => {
+          dropdown.querySelector(".dropdown-content").classList.remove("show");
+          dropdown.querySelector(".dropbtn").classList.remove("active");
+        });
+      }
+    });
+  });
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+for(i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function(){
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if(panel.style.display === "block"){
+      panel.style.display = "none";
+    }else{
+      panel.style.display = "block";
+    }
+  })
+}   
+
+const slidesContainer = document.querySelector(".slides");
+const slides = document.querySelectorAll(".slide");
+let currentIndex = 0;
+
+function showNextSlide() {
+  currentIndex = (currentIndex + 1) % slides.length;
+  slidesContainer.style.transform = `translateX(-${currentIndex * 100}%)`;
+}
+
+setInterval(showNextSlide, 3000);
+
+let btns = document.querySelectorAll('.btn_filter')
+let descs = document.querySelectorAll('.desc')
+let imgs = document.querySelectorAll('.img_filter')
+
+btns.forEach((btn,index)=>{
+    btn.onclick=()=>{
+        document.querySelector('.btn_filter.active').classList.remove('active')
+        btn.classList.add('active')
+        document.querySelector('.desc.active').classList.remove('active')
+        descs[index].classList.add('active')
+        document.querySelector('.img_filter.active').classList.remove('active')
+        imgs[index].classList.add('active')
+    }
+})
